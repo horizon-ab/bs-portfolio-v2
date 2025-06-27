60cd1e484b34ebe1b2c953c6790c78b53f81e88f
Bailey Say <baileyasay@gmail.com>
2025-06-26 18:27:20 -0700

    links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link"
+
+const navLinks = {
+    "baysalt.dev" : {
+        link : "/",
+    },
+    "blog" : {
+        link : "/blog",
+    },
+}
+
 export function NavBar() {
     return (
-        <nav className="bg-inherit p-10 text-xl">
-            baysalt.dev
+        <nav className="flex flex-row p-10 text-xl gap-10">
+            {Object.entries(navLinks).map(([name, { link }]) => {
+                return (
+                    <Link key={name} href={link}>{name}</Link>
+                )
+            })}
         </nav>
     );
 }
